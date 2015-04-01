bea27fbc672ae75ae33aa0ce69c99cbbb0b21646
JP Richardson <jprichardson@gmail.com>
2015-04-01 03:38:14 -0500

    test/linkscrape: replaced terst for assert

--- test/linkscrape.test.js
+++ test/linkscrape.test.js
@@ -1,56 +1,56 @@
-var testutil = require('testutil')
-  , fs = require('fs')
-  , linkscrape = require('../lib/linkscrape')
-  , cheerio = require('cheerio');
+var assert = require('assert')
+var fs = require('fs')
+var cheerio = require('cheerio')
+var linkscrape = require('../')
 
 describe('+ linkscrape()', function() {
   it('should return the parsed links from a page', function(done) {
     fs.readFile('./test/resources/testfile.html', function(err, data) {
       linkscrape('http://someserver.com/mypage', data.toString(), function(links, $) {
-        T (links.length === 7);
+        assert(links.length === 7);
 
-        T (links[0].href === 'http://google.com');
-        T (links[0].text === 'Google');
-        T (links[0].html === '<b>Google</b>');
-        T (links[0].element != null);
-        T (links[0].link === 'http://google.com');
+        assert(links[0].href === 'http://google.com');
+        assert(links[0].text === 'Google');
+        assert(links[0].html === '<b>Google</b>');
+        assert(links[0].element != null);
+        assert(links[0].link === 'http://google.com');
 
-        T (links[1].href === '#wat');
-        T (links[1].text === 'Link in page');
-        T (links[1].html === 'Link in page');
-        T (links[1].element != null);
-        T (links[1].link === null);
-        T ($(links[1].element).attr('class') === 'pretty');
+        assert(links[1].href === '#wat');
+        assert(links[1].text === 'Link in page');
+        assert(links[1].html === 'Link in page');
+        assert(links[1].element != null);
+        assert(links[1].link === null);
+        assert($(links[1].element).attr('class') === 'pretty');
 
-        T (links[2].href === "javascript:alert('hi');");
-        T (links[2].text === 'hi');
-        T (links[2].html === 'hi');
-        T (links[2].element != null);
-        T (links[2].link === null);
+        assert(links[2].href === "javascript:alert('hi');");
+        assert(links[2].text === 'hi');
+        assert(links[2].html === 'hi');
+        assert(links[2].element != null);
+        assert(links[2].link === null);
 
-        T (links[3].href === "alert('hello')");
-        T (links[3].text === 'hello');
-        T (links[3].html === 'hello');
-        T (links[3].element != null);
-        T (links[3].link === null);
+        assert(links[3].href === "alert('hello')");
+        assert(links[3].text === 'hello');
+        assert(links[3].html === 'hello');
+        assert(links[3].element != null);
+        assert(links[3].link === null);
 
-        T (links[4].href === "/faq/questions");
-        T (links[4].text === 'Faq');
-        T (links[4].html === 'Faq');
-        T (links[4].element != null);
-        T (links[4].link === 'http://someserver.com/faq/questions');
+        assert(links[4].href === "/faq/questions");
+        assert(links[4].text === 'Faq');
+        assert(links[4].html === 'Faq');
+        assert(links[4].element != null);
+        assert(links[4].link === 'http://someserver.com/faq/questions');
 
-        T (links[5].href === "aboutus");
-        T (links[5].text === 'About Us');
-        T (links[5].html === 'About Us');
-        T (links[5].element != null);
-        T (links[5].link === 'http://someserver.com/aboutus');
+        assert(links[5].href === "aboutus");
+        assert(links[5].text === 'About Us');
+        assert(links[5].html === 'About Us');
+        assert(links[5].element != null);
+        assert(links[5].link === 'http://someserver.com/aboutus');
 
-        T (links[6].href === undefined);
-        T (links[6].text === 'Something');
-        T (links[6].html === 'Something');
-        T (links[6].element != null);
-        T (links[6].link === null);
+        assert(links[6].href === undefined);
+        assert(links[6].text === 'Something');
+        assert(links[6].html === 'Something');
+        assert(links[6].element != null);
+        assert(links[6].link === null);
         done()
       })
     })
